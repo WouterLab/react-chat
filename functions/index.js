8c7462c54eb1193166309baf236a3cd78932c5db
WouterLab <panov135@mail.ru>
2022-08-01 15:05:51 +0700

    make it better

---
+++ functions/index.js
@@ -0,0 +1,20 @@
+const functions = require("firebase-functions");
+const Filter = require('bad-words')
+
+const admin = require('firebase-admin')
+admin.initializeApp()
+
+const db = admin.firestore()
+
+exports.detectEvilUsers = functions.firestore
+        .document('messages/{msgId}')
+        .onCreate(async (doc, ctx) => {
+        const filter = new Filter()
+        const { text, uid } = doc.data()
+
+        if (filter.isProfane(text)) {
+            const cleaned = filter.clean(text)
+            await doc.ref.update({text: `I got BANNED for saying... ${cleaned}`})
+            await db.collection('banned').doc(uid).set({})
+        }
+        })
--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useRef, useState } from 'react';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDma-8JTewg9RprdgHYBrryusyLxV84fKE",
+  authDomain: "react-realtime-chat-7c4e6.firebaseapp.com",
+  projectId: "react-realtime-chat-7c4e6",
+  storageBucket: "react-realtime-chat-7c4e6.appspot.com",
+  messagingSenderId: "413119648549",
+  appId: "1:413119648549:web:b34866eec7fba4405f00cc",
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SignOut />
       </header>
+      <section>{user ? <ChatRoom /> : <SignIn />}</section>
+    </div>
+  );
+}
+
+function SignIn() {
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    auth.signInWithPopup(provider);
+  };
+  return <button className='sign-in' onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+  return (
+    auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+  );
+}
+
+function ChatRoom() {
+  const scroller = useRef()
+  const messageRef = firestore.collection("messages");
+  const query = messageRef.orderBy("createdAt").limit(25);
+  const [messages] = useCollectionData(query, { idField: "id" });
+  const [formValue, setFormValue] = useState('')
+  const sendMessage = async (e) => {
+    e.preventDefault()
+    const { uid, photoURL } = auth.currentUser
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+    setFormValue('')
+    scroller.current.scrollIntoView({ behavior: 'smooth' })
+  }
+  return (
+    <>
+      <main>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+        <div ref={scroller}></div>
+      </main>
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} />
+        <button type='submit'>Send</button>
+      </form>
+    </>
+  );
+}
+
+function ChatMessage(props) {
+  const { text, uid, photoURL } = props.message;
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received'
+  return (
+    <div className={`message ${messageClass}`}>
+        <img src={photoURL} alt='profile' />
+       <p>{text}</p>
     </div>
   );
 }
